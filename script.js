f4d765ff3ccc1057dc169a430fe24ddc390cdcea
Gaspare <Gas9220@users.noreply.github.com>
2023-02-20 15:24:48 +0100

    Completato l'header

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Chiedo il nome all'utente
-const firstName = prompt('Insert your name...')
+//const firstName = prompt('Insert your name...')
 
 // Chiedo il cognome all'utente
-const lastName = prompt('Insert your surname...')
+//const lastName = prompt('Insert your surname...')
 
 // Chiedo il colore preferito all'utente
-const favoriteColor = prompt('Insert your favorite color...')
+//const favoriteColor = prompt('Insert your favorite color...')
 
 // Stampo in console la password generata
 console.log(`${firstName}${lastName}${favoriteColor}89`)
