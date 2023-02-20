25d13d9138af4c14bb69ebae0345a51d6756833b
Gaspare <Gas9220@users.noreply.github.com>
2023-02-20 14:45:08 +0100

    Aggiunto il prompt per l'inserimento del colore

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const name = prompt('Insert your name...')
 
 // Chiedo il cognome all'utente
 const surname = prompt('Insert your surname...')
+
+// Chiedo il cognome all'utente
+const favoriteColor = prompt('Insert your favorite color...')
