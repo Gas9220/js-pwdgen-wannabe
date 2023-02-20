b0438619d0b35e47109b1b73d7fd9b2e25de8565
Gaspare <Gas9220@users.noreply.github.com>
2023-02-20 16:07:42 +0100

    Completato il main

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // Chiedo il nome all'utente
-//const firstName = prompt('Insert your name...')
+const firstName = prompt('Insert your name...')
 
 // Chiedo il cognome all'utente
-//const lastName = prompt('Insert your surname...')
+const lastName = prompt('Insert your surname...')
 
 // Chiedo il colore preferito all'utente
-//const favoriteColor = prompt('Insert your favorite color...')
+const favoriteColor = prompt('Insert your favorite color...')
+
+// Genero la password
+const password = `${firstName}${lastName}${favoriteColor}89`
+
+// Mostro la password generata nell'HTML
+document.getElementById('password').innerHTML = password
 
 // Stampo in console la password generata
-console.log(`${firstName}${lastName}${favoriteColor}89`)
+console.log(password)
+
+// Mostro i dati inseriti nei rispettivi li
+document.getElementById('name').innerHTML = `Nome: ${firstName}`
+document.getElementById('surname').innerHTML = `Cognome: ${lastName}`
+document.getElementById('favoriteColor').innerHTML = `Colore: ${favoriteColor}`
